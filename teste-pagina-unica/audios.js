9abf36ff555dc18f94c6accc764ae7121df0de09
Rafael-Csza <1492945@sga.pucminas.br>
2023-11-23 21:26:53 -0300

    audio.js

---
+++ teste-pagina-unica/audios.js
@@ -0,0 +1,27 @@
+export default [
+    {   
+        id: 1,
+        src: "./assets/audios/01.mp3",
+        title: "Áudio 1",
+        img: "./assets/mix1.jpeg",
+        classe: "estudo, foco",
+    },
+    {
+        id: 2,
+        src: "./assets/audios/02.mp3",
+        title: "Áudio 2",
+        img: "./assets/mix2.jpeg"
+    },
+    {   
+        id: 3,
+        src: "./assets/audios/03.mp3",
+        title: "Áudio 3",
+        img: "./assets/mix3.jpeg"
+    },
+    {   
+        id: 4,
+        src: "./assets/audios/04.mp3",
+        title: "Áudio 4",
+        img: "./assets/mix1.jpeg"
+    }
+];
